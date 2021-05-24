2f750b970842c044d35788679bcb78b3cc9916e3
lerka-cherneeva <79197094+lerka-cherneeva@users.noreply.github.com>
2021-05-24 22:16:25 +0300

    Чернеева

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,23 @@
-// Copyright 2021 NNTU-CS
-
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int left = 0;
+    int right = size - 1;
+    int count = 0;
+    int middle;
+    while (left < right){
+        middle = (left + right)/2;
+        if (arr[middle] < value)
+            left = middle + 1;
+        else 
+            right = middle - 1;
+        if (arr[middle] == value){
+            while (arr[middle] == value){
+                count++;
+                left++;
+            }
+        }
+        if (count)
+            return count;
+        else
+            return 0;
+    }
 }
